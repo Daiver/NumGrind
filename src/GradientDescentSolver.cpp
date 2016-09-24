8741c74fddfcca17bb66f89876556516ddd431d8
Kirill Klimov <ra22341@ya.ru>
2016-09-24 19:16:26 +0300

    add simple logistic regression on AND operator

--- src/GradientDescentSolver.cpp
+++ src/GradientDescentSolver.cpp
@@ -6,12 +6,12 @@ void solvers::gradientDescent(const int nIters, const float stepSize, GNScalarOu
     Eigen::VectorXf grad = Eigen::VectorXf::Zero(vars.size());
     function.forwardPass(vars);
     const float err = function.value();
-    std::cout << "Initial err" << err << std::endl;
+    std::cout << "Initial err " << err << std::endl;
     for(int iter = 0; iter < nIters; ++iter){
         function.backwardPass(1, grad);
         vars -= grad * stepSize;
         function.forwardPass(vars);
         const float err = function.value();
-        std::cout << iter << ":" << err << std::endl;
+        std::cout << iter << " : " << err << std::endl;
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,67 @@
 #include "GradientDescentSolver.h"
 #include "Eigen/Core"
 
-void linearRegressionExample()
+float sigmoid(float z)
 {
-//    auto w = GNMatrixVariable(2, 1, {0, 1});
-    assert(false);//TODO: implement it
+    return 1.0/(1.0 + exp(-z));
+}
+
+float sigmoidDer(float z)
+{
+    return sigmoid(z) * sigmoid(1.0 - z);
+}
+
+void logisticRegressionExample()
+{
+    Eigen::MatrixXf data(4, 2);
+    Eigen::VectorXf targets(4);
+    data << 0, 0,
+            0, 1,
+            1, 0,
+            1, 1;
+    targets << 0, 0, 0, 1;
+
+    auto X = GNMatrixConstant(data);
+    auto y = GNMatrixConstant(targets);
+
+    auto w = GNMatrixVariable(2, 1, {0, 1});
+    auto b = GNScalarVariable(2);
+    auto f1 = GNMatrixProduct(&X, &w);
+    auto f2 = GNMatrixScalarSum(&f1, &b);
+    auto f3 = GNMatrixMapUnaryFunction<float, sigmoid, sigmoidDer>(&f2);
+    auto f4 = GNMatrixSub(&f3, &y);
+    auto f  = GNDotProduct(&f4, &f4);
+
+    Eigen::VectorXf vars = Eigen::VectorXf::Zero(3);
+
+    f.forwardPass(vars);
+    std::cout << "Err " << f.value() << std::endl;
+    solvers::gradientDescent(10, 0.2, f, vars);
+    std::cout << vars << std::endl;
+    
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-
-    auto n1 = GNVectorVariable({0, 1});
-    auto n2 = GNVectorVariable({2, 3});
-
-    auto n3 = GNVectorElementWiseProduct(&n1, &n2);
-    auto n4 = GNDotProduct(&n3, &n1);
-
-    auto graph = n4;
-
-    Eigen::VectorXf vars = utils::vec2EVecf({1, 2, 3, 4});
-    Eigen::VectorXf grad = Eigen::VectorXf::Zero(vars.size());
-    graph.forwardPass(vars);
-    graph.backwardPass(1, grad);
-
-    std::cout << graph.toString() << std::endl;
-    for(int i = 0; i < grad.size(); ++i)
-        std::cout << i << ":" << grad[i] << " ";
-    std::cout << std::endl;
+    logisticRegressionExample();
+//    std::cout << "Hello, World!" << std::endl;
+//
+//    auto n1 = GNVectorVariable({0, 1});
+//    auto n2 = GNVectorVariable({2, 3});
+//
+//    auto n3 = GNVectorElementWiseProduct(&n1, &n2);
+//    auto n4 = GNDotProduct(&n3, &n1);
+//
+//    auto graph = n4;
+//
+//    Eigen::VectorXf vars = utils::vec2EVecf({1, 2, 3, 4});
+//    Eigen::VectorXf grad = Eigen::VectorXf::Zero(vars.size());
+//    graph.forwardPass(vars);
+//    graph.backwardPass(1, grad);
+//
+//    std::cout << graph.toString() << std::endl;
+//    for(int i = 0; i < grad.size(); ++i)
+//        std::cout << i << ":" << grad[i] << " ";
+//    std::cout << std::endl;
 
     return 0;
 }
