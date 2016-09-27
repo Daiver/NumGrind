96a431618c00c6a9d584fcb8064baa8990db0382
Kirill Klimov <ra22341@ya.ru>
2016-09-27 21:26:05 +0300

    wrap utils by NumGrind namespace

--- src/utils.h
+++ src/utils.h
@@ -5,25 +5,29 @@
 #include <vector>
 #include "Eigen/Core"
 
-namespace utils {
+namespace NumGrind {
+    namespace utils {
 
-    inline Eigen::VectorXf vec2EVecf(const std::vector<float> &vec)
-    {
-        Eigen::VectorXf res(vec.size());
-        for(int i = 0; i < vec.size(); ++i)
-            res[i] = vec[i];
-        return res;
+        inline Eigen::VectorXf vec2EVecf(const std::vector<float> &vec) {
+            Eigen::VectorXf res(vec.size());
+            for (int i = 0; i < vec.size(); ++i)
+                res[i] = vec[i];
+            return res;
+        }
+
+        template<typename T>
+        T gauss(const T mean, const T std, std::default_random_engine &generator);
+
+        float gaussf(const float mean, const float std, std::default_random_engine &generator);
+
+        template<typename T>
+        Eigen::Matrix<T, -1, -1>
+        gauss(const int rows, const int cols, const T mean, const T std, std::default_random_engine &generator);
+
+        Eigen::MatrixXf gaussf(const int rows, const int cols, const float mean, const float std,
+                               std::default_random_engine &generator);
     }
-
-    template <typename T>
-    T gauss(const T mean, const T std, std::default_random_engine &generator);
-    float gaussf(const float mean, const float std, std::default_random_engine &generator);
-
-    template <typename T>
-    Eigen::Matrix<T, -1, -1> gauss(const int rows, const int cols, const T mean, const T std, std::default_random_engine &generator);
-    Eigen::MatrixXf gaussf(const int rows, const int cols, const float mean, const float std, std::default_random_engine &generator);
 }
-
 
 
 
@@ -47,20 +51,20 @@ namespace utils {
 //IMPLEMENTATIONS
 
 template <typename T>
-inline T utils::gauss(const T mean, const T std, std::default_random_engine &generator)
+inline T NumGrind::utils::gauss(const T mean, const T std, std::default_random_engine &generator)
 {
     std::normal_distribution<T> distribution(mean, std);
     return distribution(generator);
 }
 
-inline float utils::gaussf(const float mean, const float std, std::default_random_engine &generator)
+inline float NumGrind::utils::gaussf(const float mean, const float std, std::default_random_engine &generator)
 {
     std::normal_distribution<float> distribution(mean, std);
     return distribution(generator);
 }
 
 template <typename T>
-inline Eigen::Matrix<T, -1, -1> utils::gauss(const int rows, const int cols, const T mean, const T std, std::default_random_engine &generator)
+inline Eigen::Matrix<T, -1, -1> NumGrind::utils::gauss(const int rows, const int cols, const T mean, const T std, std::default_random_engine &generator)
 {
     std::normal_distribution<T> distribution(mean, std);
     Eigen::Matrix<T, -1, -1> res(rows, cols);
@@ -70,14 +74,9 @@ inline Eigen::Matrix<T, -1, -1> utils::gauss(const int rows, const int cols, con
     return res;
 };
 
-inline Eigen::MatrixXf utils::gaussf(const int rows, const int cols, const float mean, const float std, std::default_random_engine &generator)
+inline Eigen::MatrixXf NumGrind::utils::gaussf(const int rows, const int cols, const float mean, const float std, std::default_random_engine &generator)
 {
-    std::normal_distribution<float> distribution(mean, std);
-    Eigen::MatrixXf res(rows, cols);
-    for(int i = 0; i < rows; ++i)
-        for(int j = 0; j < cols; ++j)
-            res(i, j) = distribution(generator);
-    return res;
+    return gauss<float>(rows, cols, mean, std, generator);
 };
 
 
