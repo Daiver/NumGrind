3d5a0a7c92f60154a522e02a913baeed6856399b
Kirill Klimov <ra22341@ya.ru>
2016-09-29 22:29:35 +0300

    add mnist reading

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,21 +5,18 @@
 #include "solvers/GradientDescentSolver.h"
 #include "solvers/StochasticGradientDescentSolver.h"
 #include "solvers/checkgradient.h"
+#include "mnist.h"
 
-
-float sigmoid(float z)
-{
-    return (float)(1.0f/(1.0f + exp(-z)));
+float sigmoid(float z) {
+    return (float) (1.0f / (1.0f + exp(-z)));
 }
 
-float sigmoidDer(float z)
-{
-    const float sigZ =sigmoid(z);
+float sigmoidDer(float z) {
+    const float sigZ = sigmoid(z);
     return sigZ * (1.0 - sigZ);
 }
 
-void logisticRegressionOperatorAndExample02()
-{
+void logisticRegressionOperatorAndExample02() {
     using namespace NumGrind;
     using namespace NumGrind::SymbolicGraph;
 
@@ -55,8 +52,7 @@ void logisticRegressionOperatorAndExample02()
     std::cout << b.value() << std::endl;
 }
 
-void mlpOperatorOrExample01()
-{
+void mlpOperatorOrExample01() {
     using namespace NumGrind::SymbolicGraph;
     NumGrind::GraphManager gm;
 
@@ -88,7 +84,8 @@ void mlpOperatorOrExample01()
     NumGrind::solvers::SolverSettings settings;
     settings.nMaxIterations = 40;
 
-    std::cout << "is gradient ok? " << NumGrind::solvers::isGradientOk(gm.funcFromNode(&err), gm.gradFromNode(&err), vars) << std::endl;
+    std::cout << "is gradient ok? "
+              << NumGrind::solvers::isGradientOk(gm.funcFromNode(&err), gm.gradFromNode(&err), vars) << std::endl;
 
     NumGrind::solvers::gradientDescent(settings, 2.0, gm.funcFromNode(&err), gm.gradFromNode(&err), vars);
     f2.node()->forwardPass(vars);
@@ -99,8 +96,7 @@ void mlpOperatorOrExample01()
 }
 
 
-void mlpOperatorOrExample02()
-{
+void mlpOperatorOrExample02() {
     using namespace NumGrind::SymbolicGraph;
     NumGrind::GraphManager gm;
 
@@ -133,14 +129,15 @@ void mlpOperatorOrExample02()
     settings.nMaxIterations = 500;
     NumGrind::solvers::StochasticGradientDescentSolver solver(settings, 4.0);
 
-    std::cout << "is gradient ok? " << NumGrind::solvers::isGradientOk(gm.funcFromNode(&err), gm.gradFromNode(&err), vars) << std::endl;
+    std::cout << "is gradient ok? "
+              << NumGrind::solvers::isGradientOk(gm.funcFromNode(&err), gm.gradFromNode(&err), vars) << std::endl;
 
     const int nIters = 100;
     std::uniform_int_distribution<int> dist(0, data.rows() - 1);
-    for(int iter = 0; iter < nIters; ++iter){
+    for (int iter = 0; iter < nIters; ++iter) {
         const int index = dist(generator);
         const Eigen::MatrixXf sample = data.row(index);
-        const Eigen::MatrixXf label  = targets.row(index);
+        const Eigen::MatrixXf label = targets.row(index);
         X.setValue(data);
         y.setValue(targets);
         solver.makeStep(gm.funcFromNode(&err), gm.gradFromNode(&err), vars);
@@ -154,10 +151,22 @@ void mlpOperatorOrExample02()
     std::cout << "b2:" << std::endl << b2.value() << std::endl;
 }
 
+void mnistTest01() {
+    const std::string fnameMNISTDir = "/home/daiver/coding/data/mnist/";
+    const std::string fnameImagesTrain = fnameMNISTDir + "train-images-idx3-ubyte";
+    const std::string fnameLabelsTrain = fnameMNISTDir + "train-labels-idx1-ubyte";
+    const std::string fnameImagesTest  = fnameMNISTDir + "t10k-images-idx3-ubyte";
+    const std::string fnameLabelsTest  = fnameMNISTDir + "t10k-labels-idx1-ubyte";
+
+    const Eigen::MatrixXf trainData   = mnist::readMNISTImages(fnameImagesTrain);
+    const Eigen::VectorXi trainLabels = mnist::readMNISTLabels(fnameLabelsTrain);
+}
+
 int main() {
 //    logisticRegressionOperatorAndExample02();
 //    mlpOperatorOrExample01();
-    mlpOperatorOrExample02();
+//    mlpOperatorOrExample02();
+    mnistTest01();
     return 0;
 }
 
--- examples/mnist.cpp
+++ examples/mnist.cpp
@@ -2,8 +2,7 @@
 
 #include <fstream>
 
-int reverseInt (int i)
-{
+int reverseInt(int i) {
     unsigned char c1, c2, c3, c4;
 
     c1 = i & 255;
@@ -11,83 +10,64 @@ int reverseInt (int i)
     c3 = (i >> 16) & 255;
     c4 = (i >> 24) & 255;
 
-    return ((int)c1 << 24) + ((int)c2 << 16) + ((int)c3 << 8) + c4;
+    return ((int) c1 << 24) + ((int) c2 << 16) + ((int) c3 << 8) + c4;
 }
 
-/*
- * ml::DataSet readMNISTData(std::string fname)
-{
-    ml::DataSet res;
+Eigen::MatrixXf mnist::readMNISTImages(const std::string &fname) {
     std::ifstream in;
     in.open(fname.c_str(), std::ios::binary | std::ios::in);
 
     int magic;
-    in.read((char*)&magic, sizeof(magic));
+    in.read((char *) &magic, sizeof(magic));
     magic = reverseInt(magic);
-    if(magic != 2051){
+    if (magic != 2051) {
         throw std::string("bad magic");
     }
     int countOfImages;
-    in.read((char*)&countOfImages, sizeof(countOfImages));
+    in.read((char *) &countOfImages, sizeof(countOfImages));
     countOfImages = reverseInt(countOfImages);
 
     int rows;
-    in.read((char*)&rows, sizeof(rows));
+    in.read((char *) &rows, sizeof(rows));
     rows = reverseInt(rows);
 
     int cols;
-    in.read((char*)&cols, sizeof(cols));
+    in.read((char *) &cols, sizeof(cols));
     cols = reverseInt(cols);
 
     printf("images %d rows %d cols %d\n", countOfImages, rows, cols);
 
-    res.resize(countOfImages, NULL);
-    for(int i = 0; i < countOfImages; ++i){
-        ml::FeatureVec *feats = new ml::FeatureVec(rows*cols, 0);
-        for(int r = 0; r < rows; ++r){
-            for(int c = 0; c < cols; ++c){
-                unsigned char temp=0;
-                in.read((char*)&temp,sizeof(temp));
-                (*feats)[r*rows + c] = temp;
-                //printf("%f\n", (float)temp);
-            }
+    Eigen::MatrixXf res;
+    res.resize(countOfImages, rows * cols);
+    for (int i = 0; i < countOfImages; ++i) {
+        for (int j = 0; j < rows * cols; ++j) {
+            unsigned char temp = 0;
+            in.read((char *) &temp, sizeof(temp));
+            res(i, j) = temp;
         }
-        res[i] = feats;
     }
-
     in.close();
     return res;
-}
-
-std::vector<int> readMNISTLabels(std::string fname)
-{
-
-}
-*/
-Eigen::MatrixXf mnist::readMNISTImages(const std::string &fileName) {
-    return Eigen::MatrixXf();
 }
 
 Eigen::VectorXi mnist::readMNISTLabels(const std::string &fname) {
     std::ifstream in;
     in.open(fname.c_str(), std::ios::binary | std::ios::in);
     int magic;
-    in.read((char*)&magic, sizeof(magic));
+    in.read((char *) &magic, sizeof(magic));
     magic = reverseInt(magic);
 
     int countOfLabels;
-    in.read((char*)&countOfLabels, sizeof(countOfLabels));
+    in.read((char *) &countOfLabels, sizeof(countOfLabels));
     countOfLabels = reverseInt(countOfLabels);
 
     printf("magic %d %d\n", magic, countOfLabels);
 
     Eigen::VectorXi res = Eigen::VectorXi::Zero(countOfLabels);
     unsigned char tmp;
-    int counter = 0;
-    while(in.good()){
-        in.read((char*)&tmp, sizeof(tmp));
-        res[counter] = tmp;
-        counter++;
+    for(int i = 0; i < countOfLabels; ++i) {
+        in.read((char *) &tmp, sizeof(tmp));
+        res[i] = tmp;
     }
 
     in.close();
