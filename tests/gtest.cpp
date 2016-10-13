9a103b304450d73ad5734fcbc1402eafb6bdf161
Kirill Klimov <ra22341@ya.ru>
2016-10-13 08:15:58 +0300

    move test run to main section of travis.yml

--- tests/gtest.cpp
+++ tests/gtest.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    const int code =RUN_ALL_TESTS();
+    const int code = RUN_ALL_TESTS();
     std::cout << code << std::endl;
     return code;
 }
