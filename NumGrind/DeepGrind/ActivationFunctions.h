8349fab9a3eaa979c6b418963691d22e552fdf5e
daiver <ra22341@ya.ru>
2016-10-14 10:29:03 +0300

    small refactoring of DeepGrind

--- NumGrind/DeepGrind/ActivationFunctions.h
+++ NumGrind/DeepGrind/ActivationFunctions.h
@@ -5,26 +5,56 @@
 
 namespace DeepGrind {
 
-    float sigmoid(float z) {
-        return (float) (1.0f / (1.0f + exp(-z)));
-    }
+    float sigmoid(const float z);
 
-    float sigmoidDer(float z) {
-        const float sigZ = sigmoid(z);
-        return sigZ * (1.0f - sigZ);
-    }
+    float sigmoidDer(const float z);
 
-    float relu(float x) {
-        return std::max(0.0f, x);
-    }
+    float relu(const float x);
 
-    float reluDer(float x) {
-        if (x < 0.0f)
-            return 0.0f;
-        return x;
-    }
+    float reluDer(const float x);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//IMPLEMENTATIONS
+
+inline float DeepGrind::sigmoid(const float z) 
+{
+    return (float) (1.0f / (1.0f + exp(-z)));
+}
+
+inline float DeepGrind::sigmoidDer(const float z) 
+{
+    const float sigZ = sigmoid(z);
+    return sigZ * (1.0f - sigZ);
+}
+
+inline float DeepGrind::relu(const float x) 
+{
+    return std::max(0.0f, x);
+}
+
+inline float DeepGrind::reluDer(const float x) 
+{
+    if (x < 0.0f)
+        return 0.0f;
+    return x;
+}
+
 
 
 #endif //NUMGRIND_ACTIVATIONFUNCTIONS_H
